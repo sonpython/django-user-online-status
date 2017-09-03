7cbe064b6b7171630f50bb52d0bce680afd118c7
Michael Phan <sonpython@gmail.com>
2017-09-04 01:38:59 +0700

    change version to 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
  
 setup(
     name = 'django-online-status',
-    version = '0.1.1',
+    version = '0.1.2',
     packages = find_packages(),    
     author = 'Jakub Zalewski [edited by Michael Phan]',
     author_email = 'zalew7@gmail.com',    
